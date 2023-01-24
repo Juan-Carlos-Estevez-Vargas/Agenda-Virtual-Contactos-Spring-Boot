ff56eaf2198bbc58a637db2f98b67bdded1b1954
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-01-24 16:21:49 -0500

    Primer commit

---
+++ src/main/java/com/estevez/agenda/AgendaVirtualContactosApplication.java
@@ -0,0 +1,13 @@
+package com.estevez.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaVirtualContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaVirtualContactosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estevez/agenda/AgendaVirtualContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estevez.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaVirtualContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
