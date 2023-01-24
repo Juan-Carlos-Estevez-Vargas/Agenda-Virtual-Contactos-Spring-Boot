50b657e0ae355db381f2807112c9e94196f9a4aa
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-01-24 17:27:11 -0500

    Mapeo de la tabla contacto

---
+++ src/main/java/com/estevez/agenda/models/Contacto.java
@@ -0,0 +1,31 @@
+package com.estevez.agenda.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contacto")
+public class Contacto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_contacto")
+	private Integer id;
+	private String nombre;
+	
+	@Column(name = "fecha_nacimiento")
+	private LocalDate fechaNacimiento;
+	private String telefono;
+	private String email;
+	
+	@Column(name = "fecha_registro")
+	private LocalDateTime fechaRegistro;
+
+}
